c9d1351bc1c1bf00eea5e2889b7089f703310c9c
srhuang <lukyandy3162@gmail.com>
2019-11-04 15:17:01 +0800

    127 11.71/43.18

---
+++ 127 Word Ladder/ladderLength.cpp
@@ -0,0 +1,87 @@
+/*****************************************************************
+Name    :ladderLength
+Author  :srhuang
+Email   :lukyandy3162@gmail.com
+History :
+    20191101 Initial Version
+*****************************************************************/
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+
+/*==============================================================*/
+//Data structure
+
+/*==============================================================*/
+//Solution
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> q; //for BFS
+
+        //initialization
+        q.push(beginWord);
+        int ladder = 1;
+
+        while(!q.empty()){
+            //先記錄這一層(ladder)的個數
+            int n = q.size();
+
+            //handle this ladder
+            for(int i=0; i<n; i++){
+                //get from the queue
+                string word = q.front();
+                q.pop();
+
+                //check if equal to end word
+                if(word == endWord){
+                    return ladder;
+                }
+
+                //insert the next level words
+                dict.erase(word);
+
+                //find the next level words in dictionary
+                for(int j=0; j<word.size(); j++){
+                    //記錄該位置原始的字元
+                    char c = word[j];
+
+                    //嘗試更換成26個字元
+                    for(int k=0; k<26; k++){
+                        word[j] = 'a'+k;
+                        if(dict.find(word) != dict.end())
+                        {//find the word in dictionary
+                            q.push(word);
+                        }
+                    }
+
+                    word[j] = c; //回復該字元
+                }//for all possible position
+
+            }//for
+
+            ladder++;
+        }
+        //means not found
+        return 0;
+    }
+};
+
+/*==============================================================*/
+int main(int argc, char const *argv[]){
+    string beginWord("hit");
+    string endWord("cog");
+    vector<string> dict{"hot","dot","dog","lot","log","cog"};
+
+    Solution sol;
+    int answer = sol.ladderLength(beginWord, endWord, dict);
+    cout << "Answer is :" << answer <<endl;
+
+    return 0;
+}
+/*==============================================================*/
